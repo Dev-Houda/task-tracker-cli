f012e4b0c9f19fa866f78455e7db7f1cfa358f23
Dev-Houda <209240992+Dev-Houda@users.noreply.github.com>
2025-10-19 17:29:04 +0100

    Add a New Task (Create Functionality)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { readFile, writeFile } from "node:fs";
+import { readFile, writeFile, appendFile } from "node:fs";
+import { argv } from "node:process";
 
-readFile("./tasks.json", "utf8", (err, data) => {
+readFile("tasks.json", "utf8", (err, data) => {
   if (err) {
     writeFile("tasks.json", "[]", (err) => {
       if (err) {
@@ -11,8 +12,32 @@ readFile("./tasks.json", "utf8", (err, data) => {
     });
   } else {
     try {
-      JSON.parse(data);
-      console.log(JSON.parse(data));
+      console.log(data);
+      if (argv[2] === "add") {
+        const oldData = JSON.parse(data);
+        console.log(oldData);
+        console.log("creating new task object");
+        console.log(argv[3]);
+        const newTask = {
+          id: oldData.length + 1,
+          description: argv[3],
+          status: "todo",
+          createdAt: Date.now(),
+          updatedAt: Date.now(),
+        };
+        console.log(newTask);
+        console.log("pushing to data");
+        oldData.push(newTask);
+        console.log("writing to file");
+        writeFile("tasks.json", `${JSON.stringify(oldData)}`, (err) => {
+          if (err) {
+            console.error("Error appending to file:", err);
+            return;
+          }
+          console.log(`Task added successfully (ID: ${newTask.id})`);
+          console.log(oldData);
+        });
+      }
     } catch {
       console.log("Invalid file. Recreating empty tasks.json");
       writeFile("tasks.json", "[]", (err) => {
@@ -22,3 +47,8 @@ readFile("./tasks.json", "utf8", (err, data) => {
     }
   }
 });
+
+// // print process.argv
+// argv.forEach((val, index) => {
+//   console.log(`${index}: ${val}`);
+// });
