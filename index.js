853f383dd03ecc51d2f99620925dd2f13f58f345
Dev-Houda <209240992+Dev-Houda@users.noreply.github.com>
2025-10-19 20:22:49 +0100

    Listing All Tasks

--- index.js
+++ index.js
@@ -13,30 +13,53 @@ readFile("tasks.json", "utf8", (err, data) => {
   } else {
     try {
       console.log(data);
-      if (argv[2] === "add") {
-        const oldData = JSON.parse(data);
-        console.log(oldData);
-        console.log("creating new task object");
-        console.log(argv[3]);
-        const newTask = {
-          id: oldData.length + 1,
-          description: argv[3],
-          status: "todo",
-          createdAt: Date.now(),
-          updatedAt: Date.now(),
-        };
-        console.log(newTask);
-        console.log("pushing to data");
-        oldData.push(newTask);
-        console.log("writing to file");
-        writeFile("tasks.json", `${JSON.stringify(oldData)}`, (err) => {
-          if (err) {
-            console.error("Error appending to file:", err);
-            return;
-          }
-          console.log(`Task added successfully (ID: ${newTask.id})`);
+      switch (argv[2]) {
+        case "add":
+          const oldData = JSON.parse(data);
+          const currentDate = new Date();
+          const month = currentDate.getMonth() + 1;
+          const year = currentDate.getFullYear();
+          const day = currentDate.getDate();
           console.log(oldData);
-        });
+          console.log("creating new task object");
+          console.log(argv[3]);
+          const newTask = {
+            id: oldData.length + 1,
+            description: argv[3],
+            status: "todo",
+            createdAt: `${year}-${month}-${day}`,
+            updatedAt: `${year}-${month}-${day}`,
+          };
+          console.log(newTask);
+          console.log("pushing to data");
+          oldData.push(newTask);
+          console.log("writing to file");
+          writeFile("tasks.json", `${JSON.stringify(oldData)}`, (err) => {
+            if (err) {
+              console.error("Error appending to file:", err);
+              return;
+            }
+            console.log(`Task added successfully (ID: ${newTask.id})`);
+            console.log(oldData);
+          });
+          break;
+
+        case "list":
+          const allData = JSON.parse(data);
+          console.log(allData);
+          if (allData.length === 0) console.log("No tasks found.");
+          allData.forEach((task, index) => {
+            console.log(
+              `[${index + 1}] ${task.description} — ${task.status} (Created: ${
+                task.createdAt
+              })`
+            );
+          });
+          break;
+
+        default:
+          console.log("Unknown command!");
+          break;
       }
     } catch {
       console.log("Invalid file. Recreating empty tasks.json");
